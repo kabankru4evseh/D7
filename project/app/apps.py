502ffe692d7ecc6400faad6ae98022870d81c13b
kabankru4evseh <kaban-kru4e-vseh@mail.ru>
2022-04-07 17:40:37 +0300

    final

--- project/app/apps.py
+++ project/app/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+import redis
 
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+red = redis.Redis(
+    host='redis-14806.c284.us-east1-2.gce.cloud.redislabs.com',
+    port=14806,
+    password='yuGzbSllKh2tkllu4sTvNfbu0tXlIhbN',
+)
